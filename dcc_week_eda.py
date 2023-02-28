cf248947bc17e651261ff5de5929569f48b50322
brunopaiva <bruno>
2023-02-28 09:23:32 -0300

    added functions

--- dcc_week_eda.py
+++ dcc_week_eda.py
@@ -20,6 +20,59 @@ f = h5py.File(filename, 'r')
 # Get ids
 traces_ids = np.array(f['exam_id'])
 x = f['tracings']
+## get Labels
+classes = {0 :"Nenhum diagnóstico",
+           1 :"1dAVb: bloqueio atrioventricular de primeiro grau",
+           2 :"RBBB: bloqueio de ramo direito",
+           3 :"LBBB: bloqueio de ramo esquerdo",
+           4 :"SB: bradicardia sinusal",
+           5 :"AF: fibrilação atrial",
+           6 :"ST: taquicardia sinusal"}
+labels = pd.read_csv(root/"train_dccweek2023-labels.csv").set_index('exam_id').classe.replace(classes)
 
-x_batch = x[0:1024]
+labels.value_counts(normalize=True)
+
+x_batch = x[0:512]
 pickle.dump(x_batch, open(root/'sampleData.pickle', "wb"))
+
+## Explore a single data point
+idx = 0
+xi = x_batch[idx]
+xi.shape
+sid = traces_ids[idx]
+label = labels.loc[sid]
+
+colnames = ["DI","DII","DIII","AVR","AVL","AVF","V1","V2","V3","V4","V5","V6",]
+df_xi = pd.DataFrame(xi, columns=colnames)
+plt.figure(figsize=(8,12))
+for i,c in enumerate(colnames):
+    plt.subplot(6,2,i+1)
+    df_xi[c].plot.line()
+    plt.title(c)
+plt.tight_layout()
+
+x_full = f['tracings'][:]
+
+(x_full.min(), x_full.max())
+
+EPS = 1e-6
+x_batchPre = x_batch + EPS
+x_batch_pctChange = np.nan_to_num(x_batchPre[1:] / x_batchPre[:-1])
+(x_batch.min(), x_batch.max())
+
+idx = 0
+xi = x_batch_pctChange[idx]
+xi.shape
+sid = traces_ids[idx]
+label = labels.loc[sid]
+
+colnames = ["DI","DII","DIII","AVR","AVL","AVF","V1","V2","V3","V4","V5","V6",]
+df_xi = pd.DataFrame(xi, columns=colnames)
+plt.figure(figsize=(8,12))
+for i,c in enumerate(colnames):
+    plt.subplot(6,2,i+1)
+    df_xi[c].plot.line()
+    plt.title(c)
+plt.tight_layout()
+
+
